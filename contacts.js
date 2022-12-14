60d04f07b36fa348dd720cbf3154ed7f1a6e9c4a
Vasilina Biznyakova <vbiznyakova@gmail.com>
2022-12-14 17:29:56 +0100

    add cli app

---
+++ contacts.js
@@ -0,0 +1,52 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+console.log(contactsPath);
+
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(result);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  const result = await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2)
+  );
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
